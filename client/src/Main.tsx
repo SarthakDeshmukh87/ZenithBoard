64261bcac811faa7dec0bee29f3cc256dce1122c
SarthakDeshmukh87 <deshmukhsarthak23@gmail.com>
2026-01-20 09:58:22 +0530

    Initial commit: ZenithBoard collaborative canvas

--- client/src/Main.tsx
+++ client/src/Main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.tsx'
+import './index.css' // Optional: if you have global styles
 
-// This must match the ID in your index.html
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
